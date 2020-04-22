b9bcbf7bb32db5fadbfdc942b93214d518c5e8e3
skpetra <skpetra@student.math.hr>
2020-04-22 05:44:33 +0200

    [skpetra, tomarga] adding descriptions

--- functions/draw_heatmap.py
+++ functions/draw_heatmap.py
@@ -20,3 +20,22 @@ def draw_heatmap (features_category, best_features_in_category):
 
     plt.subplots(figsize=(20,15))
     sn.heatmap(best_features_in_category_dataframe.corr(), cmap='Purples', annot=True)
+    
+    
+    
+def draw_heatmap_anNot (features_category, best_features_in_category): 
+    
+    # potrebni paketi
+    import pandas as pd
+    import seaborn as sn
+    import matplotlib.pyplot as plt
+    
+    best_features_in_category_dataframe = pd.DataFrame()
+    
+    for features in features_category.columns:
+        for best_features in best_features_in_category:
+            if (features == best_features):
+                best_features_in_category_dataframe.insert(best_features_in_category_dataframe.shape[1], features, features_category[features])
+
+    plt.subplots(figsize=(20,15))
+    sn.heatmap(best_features_in_category_dataframe.corr(), cmap='Purples')
